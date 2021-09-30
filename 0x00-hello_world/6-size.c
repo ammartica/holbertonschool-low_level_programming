f87174365c9f43f74fb4e95140a69bf278a86da8
ammartica <amisadaymartinez@gmail.com>
2021-09-30 20:47:07 +0000

    task 6: sizeof()

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - prints size of various types on the computer it is compiled and run on
+ * main - prints size of different data types and modifiers
  * Return: 0
 */
 
 int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	
+
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
@@ -17,5 +17,5 @@ int main(void)
 
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
-	return(0);
+	return (0);
 }
