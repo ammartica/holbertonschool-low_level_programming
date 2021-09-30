6987e816f60ebb61f8ca01dbdd69cfe00e047356
ammartica <amisadaymartinez@gmail.com>
2021-09-30 19:59:16 +0000

    Task 4: print string with puts() function

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+/*Function main prints a string using the puts() function*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
