48a237ae3c00417d87da7425cb2234b066f1da57
ammartica <amisadaymartinez@gmail.com>
2021-09-30 20:05:00 +0000

    Third try task 4: print with puts()

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/*Function main prints a string using the puts() function*/
+/**
+ * main - prints a string using the puts() function
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
