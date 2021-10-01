ef31c3577e5e53f467a1b79c725a2873fb9b45fd
ammartica <amisadaymartinez@gmail.com>
2021-10-01 17:27:21 +0000

    task 2 try 2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,17 +2,14 @@
 
 /**
  * main - Prints the alphabet in lowercase, followed by a new line
- * Return - nothing
+ * Return: nothing
 **/
 int main(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
 		putchar(c);
-		putchar('\n');
-	}
 
 	return (0);
 }
