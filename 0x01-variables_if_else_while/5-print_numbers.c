373ef037ea918fd8b8a39bf3e47c292d5d59c361
ammartica <amisadaymartinez@gmail.com>
2021-10-01 19:38:36 +0000

    task 5: print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers from 0 to 10
+ * Return: nothing
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+		printf("%d\n", num);
+
+	return (0);
+}
