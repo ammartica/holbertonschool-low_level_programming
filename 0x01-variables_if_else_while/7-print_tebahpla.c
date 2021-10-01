0bba6f0d340139c1ab55f584bb4ed2bbca9a3423
ammartica <amisadaymartinez@gmail.com>
2021-10-01 20:45:45 +0000

    task 7: print alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet in reverse
+ * Return: nothing
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 122; letter < 97; letter--)
+	{
+		putchar(letter);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
