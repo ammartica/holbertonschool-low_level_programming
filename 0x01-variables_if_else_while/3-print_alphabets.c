8bb8596f090c8b21397ca75ab199d64307f4791d
ammartica <amisadaymartinez@gmail.com>
2021-10-01 18:28:48 +0000

    Task 3: print alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print the alphabet in lower then uppercase, followed by new line
+ * Return: nothing
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
