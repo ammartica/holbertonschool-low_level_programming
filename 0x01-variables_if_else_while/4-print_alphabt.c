5e1a2495281a74973323fb8ac14fdf116e783ff4
ammartica <amisadaymartinez@gmail.com>
2021-10-01 19:22:44 +0000

    task 4 sixth 6

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,22 +3,23 @@
 /**
  * main - Prints the alphabet in lowercase except q and e, followed by a new line
  * Return: nothing
-**/
+ **/
 int main(void)
 {
 	char c;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (c = 'a'; c < 'z'; c++)
 	{
-		putchar(c);
-
 		if (c == 'e' || c == 'q')
 		{
 			continue;
 		}
+		else
+		{
+			putchar(c);
+		}
 	}
 
 	putchar('\n');
-
 	return (0);
 }
