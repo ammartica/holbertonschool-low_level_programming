e6ab6c2ad48bb643177d0d5c37b07a8d8944cc22
ammartica <amisadaymartinez@gmail.com>
2021-10-06 19:07:41 +0000

    Task 10: function adds two ints and returns the result. Additionally, edit to main.h file

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,6 +12,10 @@ int print_sign(int n);
 
 int _abs(int num);
 
-int print_last_digit(int r);
+int print_last_digit(int num);
 
 void jack_bauer(void);
+
+void times_table(void);
+
+int add(int num1, int num2);
