f4e570fa658b610d28d7d5c92c810fc78de613ba
ammartica <amisadaymartinez@gmail.com>
2021-10-06 17:44:57 +0000

    Task 8: prints every minute of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,10 +1,24 @@
 #include "main.h"
-#include <time.h>
 
 /**
  * jack_bauer - prints every minute of the day from 00:00 to 23:59
  * Return: nothing
-**/
+ **/
 void jack_bauer(void)
 {
+	int hours;
+	int mins;
 
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (mins = 0; mins < 60; mins++)
+		{
+			_putchar((hours / 10) + '0');
+			_putchar((hours % 10) + '0');
+			_putchar(':');
+			_putchar((mins / 10) + '0');
+			_putchar((mins % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
