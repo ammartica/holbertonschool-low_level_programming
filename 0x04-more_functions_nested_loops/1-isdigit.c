7603b377141722d8a945cb4a02db2813019ecf8d
ammartica <amisadaymartinez@gmail.com>
2021-10-07 17:36:32 +0000

    task 0: checks for uppercase letter

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit 0-9
+ * @c: digit to be checked
+ * Return: 1 if c is a digit or 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return (0);
+}
