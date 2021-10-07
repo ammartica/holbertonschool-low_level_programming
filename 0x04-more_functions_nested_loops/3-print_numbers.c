b51fb0e036864c9d30ae6fa68b9a0f7398c7bbb4
ammartica <amisadaymartinez@gmail.com>
2021-10-07 17:39:30 +0000

    task 3: function prints numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - Prints numbers from 0-9
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
