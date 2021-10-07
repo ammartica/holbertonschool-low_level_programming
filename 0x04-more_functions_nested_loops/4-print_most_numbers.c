e4318f3b02821a86453a620e7d78f3d01cfbf7b6
ammartica <amisadaymartinez@gmail.com>
2021-10-07 17:40:22 +0000

    task 4: function prints numbers 0-9 except 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers from 0-9 except for 2 and 4
+ * Return: nothing
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		if (i == 50 || i == 52)
+			continue;
+		else
+			_putchar(i);
+	}
+
+	_putchar('\n');
+}
