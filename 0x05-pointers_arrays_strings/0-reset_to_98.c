9ee280a356885a043bf5c6f98d9372f792cff9e6
ammartica <amisadaymartinez@gmail.com>
2021-10-11 16:17:35 +0000

    task 0: take pointer and update value to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - takes pointer to int and updates value to 98
+ * @*n: holds pointer value
+ * Returns: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
