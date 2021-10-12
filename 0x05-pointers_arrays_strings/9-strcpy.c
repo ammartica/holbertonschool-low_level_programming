19c3dd2863142259368767119e343e6e0b3118ef
ammartica <amisadaymartinez@gmail.com>
2021-10-12 17:14:51 +0000

    task 9: function that copies string

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcpy - copies string to buffer
@@ -8,12 +9,6 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	int i = 0;
-
-	while (src[i] != '\0')
-	{
-		dest[i] = src[i];
-		i++;
-	}
+	strcpy(src, dest);
 	return (dest);
 }
