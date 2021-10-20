120789b297297bd82a207507d77c3a5c8c94b689
ammartica <amisadaymartinez@gmail.com>
2021-10-20 14:49:46 +0000

    task 2: returns the length of a string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: string to verify the length of
+ * Return: length of string
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+				}
