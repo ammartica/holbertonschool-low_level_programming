64eaee7ffeaa7fe4c99225271f54087be85976bd
ammartica <amisadaymartinez@gmail.com>
2021-10-22 19:00:26 +0000

    task 1: prints number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments passed into it
+ * @argc: counter of arguments
+ * @argv: vector of arguments
+ * Return: 0 if correct
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc - 1; i++)
+	;
+		printf("%d\n", i);
+	return (0);
+}
