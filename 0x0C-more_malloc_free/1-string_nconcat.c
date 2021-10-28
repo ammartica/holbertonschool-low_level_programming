110b77110d925863bb4e4c5a3692925809a8fb98
ammartica <amisadaymartinez@gmail.com>
2021-10-28 18:00:17 +0000

    ADD 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,44 +13,30 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k, l;
 	char *a;
 
-	/* length of first string */
 	if (s1 == NULL)
 		i = 0;
-
 	else
 	{
 		for (i = 0; s1[i]; i++)
 			;
 	}
-
-	/* length of second string */
 	if (s2 == NULL)
 		j = 0;
-
 	else
 	{
 		for (j = 0; s2[j]; j++)
 			;
 	}
-
 	if (j > n)
 		j = n;
-
-	/* allocate memory */
 	k = i + j + 1;
 	a = malloc(sizeof(char) * k);
-
-	/* concatenate strings */
 	if (a == NULL)
 		return (NULL);
-
 	for (l = 0; l < i; l++)
 		a[l] = s1[l];
-
 	for (l = 0; l < j; l++)
 		a[i + l] = s2[l];
-
 	a[i + j] = '\0';
-
 	return (a);
 }
