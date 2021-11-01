ddf535e05e3703f22f19bc9eec71b3295d440853
ammartica <amisadaymartinez@gmail.com>
2021-11-01 22:12:47 +0000

    added function prototypes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -7,12 +7,16 @@
  * @age: age of dog
  * @owner: name of dog owner
  */
-
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif
