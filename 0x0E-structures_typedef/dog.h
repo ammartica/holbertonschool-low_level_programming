636d8f9f402b18d29cae03ab83108d088b801920
ammartica <amisadaymartinez@gmail.com>
2021-11-01 21:45:45 +0000

    ADD file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - structure for a dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of dog owner
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif
