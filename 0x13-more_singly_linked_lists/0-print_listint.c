f321e0e939e85bde1844a6b03a0680a90003d085
ammartica <amisadaymartinez@gmail.com>
2021-11-15 14:47:39 +0000

    ADD file

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of a listint_t list
+ * @h: list to be printed
+ *
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	unsigned int i;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
