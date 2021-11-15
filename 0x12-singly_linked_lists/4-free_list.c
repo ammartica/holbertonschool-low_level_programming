0e80ebdc44467ca93547170d4a5ae28dfc7b3211
ammartica <amisadaymartinez@gmail.com>
2021-11-15 14:04:52 +0000

    fixing git issue

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list
+ * @head: list to be freed
+ *
+ * Return: VOID
+ */
+void free_list(list_t *head)
+{
+	list_t *temp, *next;
+
+	temp = head;
+
+	while(temp)
+	{
+		next = temp->next;
+		free(temp->str);
+		free(temp);
+		temp = next;
+	}
+}
+
