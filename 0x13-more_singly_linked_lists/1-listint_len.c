f141554075fbc97f44ef031e88598bb39cb49aa0
ammartica <amisadaymartinez@gmail.com>
2021-11-15 14:52:23 +0000

    ADD file

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - iterates through elements of a linked list
+ * @h: list to iterate through
+ *
+ * Return: total elements in linked list
+ */
+size_t listint_len(const listint_t *h)
+{
+	unsigned int i = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
