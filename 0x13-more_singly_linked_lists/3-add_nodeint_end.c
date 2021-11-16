79d53f08d3683900e4b507c2d9624a3dcf2fc0d7
ammartica <amisadaymartinez@gmail.com>
2021-11-16 13:45:49 +0000

    added loop

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,11 +11,14 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	listint_t *new = malloc(sizeof(listint_t)), *tmp;
 
-	new->n = n;
-	new->next = NULL;
-
 	if (new == NULL)
 		return (NULL);
+
+	if (head == NULL)
+		return (new);
+
+	new->n = n;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
